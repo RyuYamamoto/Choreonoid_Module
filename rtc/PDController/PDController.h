e9bb307a3496a809f94d39a31bd0cf0dd25aa731
RyuYamamoto <s1226120ZA@s.chibakoudai.jp>
2016-02-02 19:46:58 +0900

    fixed indent

--- rtc/PDController/PDController.h
+++ rtc/PDController/PDController.h
@@ -12,37 +12,37 @@
 
 class PDController : public RTC::DataFlowComponentBase
 {
-public:
-    PDController(RTC::Manager* manager);
-    ~PDController();
-	void ReadGain(size_t numJoints,std::vector<double> &pgain,std::vector<double> &dgain);
+	public:
+		PDController(RTC::Manager* manager);
+		~PDController();
+		void ReadGain(size_t numJoints,std::vector<double> &pgain,std::vector<double> &dgain);
 
-    virtual RTC::ReturnCode_t onInitialize();
-    virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
-    virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
-    virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+		virtual RTC::ReturnCode_t onInitialize();
+		virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+		virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+		virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
 
-protected:
-    // DataInPort declaration
-    RTC::TimedDoubleSeq m_angle;
-    RTC::InPort<RTC::TimedDoubleSeq> m_angleIn;
-    // DataOutPort declaration
-    RTC::TimedDoubleSeq m_torque;
-    RTC::OutPort<RTC::TimedDoubleSeq> m_torqueOut;
+	protected:
+		// DataInPort declaration
+		RTC::TimedDoubleSeq m_angle;
+		RTC::InPort<RTC::TimedDoubleSeq> m_angleIn;
+		// DataOutPort declaration
+		RTC::TimedDoubleSeq m_torque;
+		RTC::OutPort<RTC::TimedDoubleSeq> m_torqueOut;
 
-private:
-	std::vector<double> pgain;
-	std::vector<double> dgain;
-    std::vector<double> angleRef;
-	std::vector<double> q_old;
-	std::vector<double> q_old_ref;
-    double dq_old;
-	double dt;
+	private:
+		std::vector<double> pgain;
+		std::vector<double> dgain;
+		std::vector<double> angleRef;
+		std::vector<double> q_old;
+		std::vector<double> q_old_ref;
+		double dq_old;
+		double dt;
 };
 
 extern "C"
 {
-    DLL_EXPORT void RobotTorqueControllerRTCInit(RTC::Manager* manager);
+	DLL_EXPORT void RobotTorqueControllerRTCInit(RTC::Manager* manager);
 };
 
 #endif
